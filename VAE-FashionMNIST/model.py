fcfee4295d0e2e5c4b8b24ed79fb159376040515
YOS YOHANNES <yos.yohannes@gmail.com>
2024-04-02 20:49:38 +0800

    Simple VAE

---
+++ VAE-FashionMNIST/model.py
@@ -0,0 +1,43 @@
+import torch
+import torch.utils.data
+from torch import nn
+from torch.nn import functional as F
+
+
+# --- defines the model --- #
+class VAE(nn.Module):
+    def __init__(self, z_dim):
+        super().__init__()
+        # self.encoder = nn.Linear(784, 500)
+        self.encoder = nn.Sequential(nn.Conv2d(1, 128, kernel_size=3, stride=2),
+                                     nn.ReLU(),
+                                     nn.Conv2d(128, 512, kernel_size=3, stride=2),
+                                     nn.ReLU(),
+                                     nn.Flatten(),
+                                     nn.Linear(18432, 500))
+        self.mu = nn.Linear(500, z_dim)
+        self.var = nn.Linear(500, z_dim)
+        self.fc1 = nn.Linear(z_dim, 500)
+        self.fc2 = nn.Linear(500, 784)
+
+    def encode(self, x):
+        h1 = F.relu(self.encoder(x))
+        mu = self.mu(h1)
+        logvar = self.var(h1)
+        return mu, logvar
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.rand_like(std)
+        return mu + eps*std
+
+    def decode(self, z):
+        h3 = F.relu(self.fc1(z))
+        return torch.sigmoid(self.fc2(h3))
+
+    def forward(self, x):
+        # x: [batch size, 1, 28,28] -> x: [batch size, 784]
+        # x = x.view(-1, 784)
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
---
+++ VAE-FashionMNIST/train.py
@@ -0,0 +1,37 @@
+import torch
+from torch.nn import functional as F
+from torch import optim
+
+
+# --- defines the loss function --- #
+def loss_function(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+    KLD = 0.5 * torch.sum(mu.pow(2) + logvar.exp() - logvar - 1)
+    return BCE + KLD
+
+# --- train and test --- #
+def train_model(model, train_loader, epoch, lr, log_interval, device):
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
+    model.train()
+    train_loss = 0
+    for batch_idx, (data, label) in enumerate(train_loader):
+        # data: [batch size, 1, 28, 28]
+        # label: [batch size] -> we don't use
+        optimizer.zero_grad()
+        data = data.to(device)
+        recon_data, mu, logvar = model(data)
+        loss = loss_function(recon_data, data, mu, logvar)
+        loss.backward()
+        cur_loss = loss.item()
+        train_loss += cur_loss
+        optimizer.step()
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100.*batch_idx / len(train_loader),
+                cur_loss/len(data)))
+
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+        epoch, train_loss / len(train_loader.dataset)
+    ))
